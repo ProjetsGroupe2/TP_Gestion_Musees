cd13b4e3b14bcfb6d03ea21c4b039fe46f434289
VAUTIER Florian <F.Vautier@iia-laval.fr>
2015-04-17 14:36:15 +0200

    Affichageet Main

--- GestionMusees/Affichage.c
+++ GestionMusees/Affichage.c
@@ -10,44 +10,25 @@ void Color(int couleurDuTexte, int couleurDeFond) // fonction d'affichage de cou
 
 void Affichage()
 {
-	{
-		Color(0, 0); //Noir, Noir
-		printf("             ");
-	}
-	{
-		Color(1, 1); //Bleu
-		printf("                                       \n");
-	}
-	{
-		Color(0, 0); //Noir, Noir
-		printf("             ");
-	}
-	{
-		Color(1, 1); //Bleu
-		printf("                                       \n");
-	}
-	{
-		Color(0, 0); //Noir, Noir
-		printf("             ");
-	}
-	{
-		Color(1, 15); //Bleu
-		printf("        Bibliotheques de France        \n");
-	}
-	{
-		Color(0, 0); //Noir, Noir
-		printf("             ");
-	}
-	{
-		Color(1, 1); //Bleu
-		printf("                                       \n");
-	}
-	{
-		Color(0, 0); //Noir, Noir
-		printf("             ");
-	}
-	{
-		Color(1, 1); //Bleu
-		printf("                                       \n");
-	}
+	Color(0, 0); //Noir, Noir
+	printf("\n\n                 ");
+	Color(1, 1); //Bleu, Bleu
+	printf("                                           \n");
+	Color(0, 0); //Noir, Noir
+	printf("                 ");
+	Color(1, 1); //Noir, Bleu
+	printf("                                           \n");
+	Color(0, 0); //Noir, Noir
+	printf("                 ");
+	Color(0, 15); //Noir, Bleu
+	printf("          Bibliotheques de France          \n");
+	Color(0, 0); //Noir, Noir
+	printf("                 ");
+	Color(1, 12); //Noir, Bleu
+	printf("                                           \n");
+	Color(0, 0); //Noir, Noir
+	printf("                 ");
+	Color(1, 12); //Noir, Bleu
+	printf("                                           \n");
+	Color(15, 0); //Noir, Noir
 }
--- GestionMusees/Main.c
+++ GestionMusees/Main.c
@@ -5,8 +5,39 @@
 #include "Affichage.h"
 
 void main()
+
 {
 	Affichage();
 	system("pause>nul");
-	return 0;
-}
+
+	int choix = 0;
+	do
+	{
+		system("cls");
+		printf(" 1er - Menu Principal ");
+		printf(" 2eme - Gerer des Musées ");
+		printf(" 3eme - Rechercher des Musées ");
+		printf(" 4eme - Sauvegarder ");
+		scanf("%d", &choix);
+
+		int choix = 0;
+		switch (choix)
+		{
+		case 1:
+			printf("Rouge");
+			break;
+		case 2:
+			printf("Vert");
+			break;
+		case 3:
+			printf("Bleu");
+			break;
+		case 4:
+			printf("Blanc");
+			break;
+		}
+	} while (choix != 0);
+		system("pause>nul");
+		return 0;
+	}
+
