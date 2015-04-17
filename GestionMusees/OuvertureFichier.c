bef773fd4e137fa5a49a881d0928a05f9662f232
Marteea <martin.amicel@gmail.com>
2015-04-17 16:11:28 +0200

    Afficher le fichier

--- GestionMusees/OuvertureFichier.c
+++ GestionMusees/OuvertureFichier.c
@@ -3,17 +3,30 @@
 
 
 void ouvertureFichier()
-{
+{	
+	char lettre = " ";
 	FILE* fichier = NULL;
 	fichier = fopen("biblio.csv", "r+");
 
+	// Verifie si le fichier s'est bien ouvert
 	if (fichier == NULL)
 	{
 		printf("Erreur lors de l'ouverture du fichier personnes.csv");
 	}
+	
+	// Affiche le fichier
 	else
 	{
-		printf("Le fichier a bien ete cree");
+		printf("Le fichier a bien ete cree \n ");
+		
+		lettre = fgetc(fichier);
+		do
+		{
+			lettre = fgetc(fichier);
+			printf("%c", lettre);
+		} while (lettre != EOF);
+
+
 	}
 
 }
