878f70c51826893265d97c228c9d61f959a2abe2
Marteea <martin.amicel@gmail.com>
2015-04-17 13:38:20 +0200

    Merge branch 'master' of https://github.com/ProjetsGroupe2/TP_Gestion_Musees
    Conflicts:
    	GestionMusees/GestionMusees.vcxproj
    	GestionMusees/GestionMusees.vcxproj.filters
    	GestionMusees/Main.c

--- GestionMusees/Main.c
+++ GestionMusees/Main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 #include "Departement.h"
 #include "GestionDonnees.h"
 #include "Musee.h"
 #include "Region.h"
 #include "Ville.h"
+=======
+#include <windows.h>
+#include "GestionDonnes.h"
+#include "Affichage.h"
+>>>>>>> 60f9f78d612f2ab882d5376210ff64589cd95509
 =======
 #include <windows.h>
 #include "GestionDonnes.h"
