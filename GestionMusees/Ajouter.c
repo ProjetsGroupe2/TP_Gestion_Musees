ecf0902287787d22f2b833397553bfa41e94418f
Quentinbd <q.boisnard@iia-laval.fr>
2015-06-11 23:19:46 +0200

    fonction ajouter (en cours)

---
+++ GestionMusees/Ajouter.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Ville.h"
+#include "Region.h"
+#include "Musee.h"
+#include "Departement.h"
+
+void AjouterMusee(MUSEE* ptrmusee)
+
+
+FILE* fichier = NULL;
+fichier = fopen("biblio.csv", "r+");
+
+if (fichier == NULL)
+{
+	printf("Erreur lors de l'ouverture du fichier personnes.csv");
+}
+
+
+fputs("", fichier);
+
+fprintf(fichier, " ", test);
+
+
+/*
+main(){
+	char* data[255];
+	printf("Saisir nom Musee");
+	scanf("%s", &data);
+	for (int i = 0; i<n; i++){
+
+
+		printf("Musee       :");
+		scanf("%s", &T[i].Nom);
+		printf("Ville    :");
+		scanf("%s", &T[i].Adresse);
+		printf("Departement   :");
+		scanf("%s", &T[i].Departement);
+		printf("Region	:");
+		scanf("%s", &T[i].Region);
+		
+		tabRegion[i].id, tabRegion[i].nom;
+		tabDepartement;
+		tabVille;
+		tabMusee;
+
+
+
+	}
+
+	printf("\n\tLe Nouveau Musee est \n");                         /*Fin du Tri*/
+//	for (i = 0; i<n; i++){
+
+//		printf("\nMusee %s", T[i].Nom);
+//		printf("\nVille %s", T[i].Adresse);
+//		printf("\nDepartement %s", T[i].Departement);
+//		printf("\nRegion %s", T[i].Region);
+
+}
+
+
+
+//	getchar();
+
+
+//	getchar();
+}
+fclose(fichier);
+
+*/
