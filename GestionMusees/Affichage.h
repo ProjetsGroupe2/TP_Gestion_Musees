33fd6910e2d55a626e3023fa5dcb0f98a9c7c85d
Marteea <martin.amicel@gmail.com>
2015-06-17 00:49:58 +0200

    Fonction Recherche terminée

--- GestionMusees/Affichage.h
+++ GestionMusees/Affichage.h
@@ -4,7 +4,7 @@ void affichage();
 void affichageMenu(int* choix);
 void MenuGererMusees(Region * tabRegion, Departement* tabDepartement, Ville* tabVille, Musee* tabMusee, int *nbRegion, int * nbDep, int * nbVille, int * nbMusee);
 //void MenuAjouterMusee();
-////void ModifierMusee();
+//void ModifierMusee();
 void SupprimerMusee();
 void MenuRechercherMusee();
 void RechercheRegion();
